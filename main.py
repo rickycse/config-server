33883de619ed08b4e1bf32f782737badd5d3f077
Ricky Tran <rickie.tran7@gmail.com>
2025-09-05 00:46:20 -0400

    added api auth

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from fastapi.encoders import jsonable_encoder
+
+from auth import verify_internal_secret
 
 load_dotenv()
 
@@ -32,22 +34,26 @@ def get_content(slug):
 
     return jsonable_encoder(result)
 
-@app.get('/content/static')
+@app.get('/content/static', dependencies=[Depends(verify_internal_secret)])
 def static_content():
     return get_content("static")
 
-@app.get('/content/problems')
+@app.get('/content/problems', dependencies=[Depends(verify_internal_secret)])
 def problems():
     return get_content("problems")
 
-@app.get('/content/safety')
+@app.get('/content/safety', dependencies=[Depends(verify_internal_secret)])
 def safety():
     return get_content("safety")
 
-@app.get('/content/filaments')
+@app.get('/content/filaments', dependencies=[Depends(verify_internal_secret)])
 def filaments():
     return get_content("filaments")
 
-@app.get('/content/printers')
+@app.get('/content/printers', dependencies=[Depends(verify_internal_secret)])
 def filament():
     return get_content("printers")
+
+@app.get('/content/concerns', dependencies=[Depends(verify_internal_secret)])
+def concerns():
+    return get_content("concerns")
